363eab60701bd124c7accb92be2dce4f8822c7cb
fpaniaguajava <fernando.paniagua.java@gmail.com>
2026-03-02 12:35:27 +0100

    Proyecto terminado

--- src/main/java/com/cursogetafe/AvePersistenceManager.java
+++ src/main/java/com/cursogetafe/AvePersistenceManager.java
@@ -1,6 +1,11 @@
 package com.cursogetafe;
 
+
+
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class AvePersistenceManager {
     static Session session=null;
@@ -17,25 +22,47 @@ public class AvePersistenceManager {
     }
 
     public static void create(Ave ave) {
-        //Inicializa la sesión (por si acaso)
         AvePersistenceManager.startSession();
         AvePersistenceManager.session.beginTransaction();
-        AvePersistenceManager.session.merge(ave);//Llamada al guardar
+        AvePersistenceManager.session.persist(ave);
         AvePersistenceManager.session.getTransaction().commit();
-        System.out.println("¡Ave guardada con éxito!");
+        System.out.println("¡Ave creada con éxito!");
     }
     public static Ave update(Ave ave) {
-        return null;
+        AvePersistenceManager.startSession();
+        AvePersistenceManager.session.beginTransaction();
+        AvePersistenceManager.session.merge(ave);
+        AvePersistenceManager.session.getTransaction().commit();
+        System.out.println("¡Ave modificada con éxito!");
+        return ave;
     }
     public static Ave read(String especie) {
         AvePersistenceManager.startSession();
         Ave aveLeida = AvePersistenceManager.session.find(Ave.class, especie);
         return aveLeida;
     }
-    public static void delete(String especie) {
-
+    public static List<Ave> readAll(){
+        AvePersistenceManager.startSession();
+        Query query = session.createQuery("FROM Ave");
+        List<Ave> aves = query.getResultList();
+        return aves;
+    }
+    public static void delete(String especie) throws Exception {
+        AvePersistenceManager.startSession();
+        Ave aveBorrable = AvePersistenceManager.session.find(Ave.class, especie);
+        if (aveBorrable==null) {
+            throw new Exception("Ave no encontrada");
+        }
+        AvePersistenceManager.session.beginTransaction();
+        AvePersistenceManager.session.remove(aveBorrable);
+        AvePersistenceManager.session.getTransaction().commit();
+        System.out.println("¡Ave eliminada con éxito!");
     }
     public static void delete(Ave ave) {
-
+        AvePersistenceManager.startSession();
+        AvePersistenceManager.session.beginTransaction();
+        AvePersistenceManager.session.remove(ave);
+        AvePersistenceManager.session.getTransaction().commit();
+        System.out.println("¡Ave eliminada con éxito!");
     }
 }
--- src/main/java/com/cursogetafe/Main.java
+++ src/main/java/com/cursogetafe/Main.java
@@ -1,5 +1,7 @@
 package com.cursogetafe;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,7 +10,25 @@ public class Main {
         AvePersistenceManager.startSession();
 
         //create
-        read();
+
+        // ave = read();
+        // System.out.prinltn(ave);
+
+        //update();
+
+        //delete();
+
+        //deleteByEspecie("Milano");
+
+        //create();
+
+        //deleteByEspecie("Milano");
+
+        create();
+        List<Ave> aves = readAll();
+        aves.forEach(ave -> {
+            System.out.println(ave);
+        });
 
         //Finalizar sesión
         AvePersistenceManager.finishSession();
@@ -17,8 +37,28 @@ public class Main {
         Ave ave1 = new Ave("Milano", "Pardo", 2.3, true);
         AvePersistenceManager.create(ave1);
     }
-    static void read(){
+    static Ave read(){
         Ave ave = AvePersistenceManager.read("Milano");
-        System.out.println(ave);
+        return ave;
     }
+    static List<Ave> readAll(){
+        List<Ave> aves = AvePersistenceManager.readAll();
+        return aves;
+    }
+    static void update(){
+        Ave aveModificada = AvePersistenceManager.update(new Ave("Milano", "Verde", 3, true));
+        System.out.println(aveModificada);
+    }
+    static void delete(){
+        Ave ave = AvePersistenceManager.read("Milano");
+        AvePersistenceManager.delete(ave);
+    }
+    static void deleteByEspecie(String especie) {
+        try {
+            AvePersistenceManager.delete(especie);
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
 }
